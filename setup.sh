bd0f0057632554c66da1508464df821b2eb8401b
OliveiraEdu <40703876+OliveiraEdu@users.noreply.github.com>
2023-10-05 16:50:31 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,10 @@ cd ~/Git
 git clone https://github.com/hyperledger/iroha.git --branch iroha2-stable
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source "$HOME/.cargo/env"
+rustup install nightly-2023-06-25
+rustup target add wasm32-unknown-unknown --toolchain nightly-2023-06-25
+rustup override set nightly-2023-06-25
+rustup component add rust-src --toolchain nightly-2023-06-25
 cd ~/Git/iroha
 cargo build -p iroha_client_cli --release
 mkdir -p test_docker
