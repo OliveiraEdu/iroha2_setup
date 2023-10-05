42f615ea4fa3acf55680fc35184eb0b1bdbc1bc8
OliveiraEdu <40703876+OliveiraEdu@users.noreply.github.com>
2023-10-05 07:46:59 -0300

    Create rust_update.sh

---
+++ rust_update.sh
@@ -0,0 +1,4 @@
+rustup install nightly-2023-06-25
+rustup target add wasm32-unknown-unknown --toolchain nightly-2023-06-25
+rustup override set nightly-2023-06-25
+rustup component add rust-src --toolchain nightly-2023-06-25
