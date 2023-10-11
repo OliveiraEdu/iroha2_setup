4be862bfe9c597786cb3997c0529cee6eaa6a579
OliveiraEdu <40703876+OliveiraEdu@users.noreply.github.com>
2023-10-11 19:36:51 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,26 +2,26 @@ apt update
 apt-get install -y libssl-dev curl build-essential pkg-config iputils-ping nano git pip patchelf
 mkdir -p ~/Git
 cd ~/Git
-git clone https://github.com/hyperledger/iroha.git --branch iroha2-stable
+git clone https://github.com/hyperledger/iroha.git --branch iroha2-lts
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source "$HOME/.cargo/env"
 
-#rustup install nightly-2023-06-25
-#rustup target add wasm32-unknown-unknown --toolchain nightly-2023-06-25
-#rustup override set nightly-2023-06-25
-#rustup component add rust-src --toolchain nightly-2023-06-25
+rustup install nightly-2023-06-25
+rustup target add wasm32-unknown-unknown --toolchain nightly-2023-06-25
+rustup override set nightly-2023-06-25
+rustup component add rust-src --toolchain nightly-2023-06-25
 
 # --- Installing the Bash Client
-#cd ~/Git/iroha
-#cargo build -p iroha_client_cli --release
-#mkdir -p test_docker
-#cp ./configs/client/config.json test_docker/
-#cp ./target/release/iroha_client_cli test_docker/
-#cd ~/Git/iroha/test_docker
+cd ~/Git/iroha
+cargo build -p iroha_client_cli --release
+mkdir -p test_docker
+cp ./configs/client/config.json test_docker/
+cp ./target/release/iroha_client_cli test_docker/
+cd ~/Git/iroha/test_docker
 
 # --- Installing the Python Client
-git clone https://github.com/hyperledger/iroha-python/ --branch iroha2-edge
-cd iroha-python
-pip install maturin
-maturin build
+#git clone https://github.com/hyperledger/iroha-python/ --branch iroha2-edge
+#cd iroha-python
+#pip install maturin
+#maturin build
 
