e975727ef8da344065b4d2a15ac207bf61dcd3c2
OliveiraEdu <40703876+OliveiraEdu@users.noreply.github.com>
2023-10-13 14:39:51 +0000

    Create account_register2.sh

---
+++ account_register2.sh
@@ -0,0 +1,5 @@
+./iroha_client_cli account register \
+    --id="gryphon@looking_glass" \
+    --key="ed0120aa732e5a5dfdebe1c417dc1dfbf172857f6797af35acd1efd625b8688523bd04"
+
+#Private Key: "cd20649f90fca5a5cd282f078b43ce4415a2339b01221a61ce8fba639b683583aa732e5a5dfdebe1c417dc1dfbf172857f6797af35acd1efd625b8688523bd04" 
