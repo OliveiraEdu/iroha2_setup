323af7ad5e66236aabc15105c690a3743ffde217
OliveiraEdu <40703876+OliveiraEdu@users.noreply.github.com>
2023-10-14 19:16:49 -0300

    Update account_register_whiterabbit.sh

--- account_register_whiterabbit.sh
+++ account_register_whiterabbit.sh
@@ -2,3 +2,9 @@
 #Public key (multihash): ed012072ab14e864d958548a9401e317ac973ba1e1f341abcb4b90eb039ddc5944e87b
 #Private key: b64d5bb1f985fd464d7ea816302920ed4b8c03e890dc417899b16dede718f4d672ab14e864d958548a9401e317ac973ba1e1f341abcb4b90eb039ddc5944e87b
 #Digest function: ed25519
+
+./iroha_client_cli account register \
+    --id="white_rabbit@looking_glass" \
+    --key="ed012072ab14e864d958548a9401e317ac973ba1e1f341abcb4b90eb039ddc5944e87b"
+
+
