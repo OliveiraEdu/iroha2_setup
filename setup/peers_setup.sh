8a4db229090f05bd7845be794ee498f0beb10cac
OliveiraEdu <40703876+OliveiraEdu@users.noreply.github.com>
2023-10-19 10:31:26 -0300

    Create peers_setup.sh

---
+++ setup/peers_setup.sh
@@ -0,0 +1,15 @@
+#succesfull setup for Iroha Python 17OCT2023
+apt update
+apt-get install -y libssl-dev curl build-essential pkg-config iputils-ping nano git pip patchelf nano
+mkdir -p ~/Git
+cd ~/Git
+git clone https://github.com/hyperledger/iroha.git --branch iroha2-lts
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source "$HOME/.cargo/env"
+rustc --version
+cargo -V
+cd ~/Git/iroha
+git status
+git checkout 9273a912c8a97f9ce1bfaa302c7192554f7abf00
+git status
+cargo build -p iroha_client_cli --release
