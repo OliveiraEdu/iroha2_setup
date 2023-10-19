4587f3cd18e66c6f9ee8f17122d09f44bbc8c721
OliveiraEdu <40703876+OliveiraEdu@users.noreply.github.com>
2023-10-19 16:48:58 -0300

    Update query_FindAssetById.sh

--- python_scripts/query_FindAssetById.sh
+++ python_scripts/query_FindAssetById.sh
@@ -14,54 +14,8 @@ from iroha2.data_model.query.asset import FindAssetById
 from iroha2.data_model.query import Query
 
 
-def wait_for_tx(cl: Client, hash: str):
-    filter = FilterBox(
-        pipeline.EventFilter(
-            entity_kind=pipeline.EntityKind.Transaction(),
-            status_kind=None,
-            hash=None,
-        ))
-
-    listener = cl.listen(filter)
-
-    for event in listener:
-        if isinstance(event, Event.Pipeline) and event.hash == hash:
-            if isinstance(event.status, pipeline.Status.Committed):
-                return
-            elif isinstance(event.status, pipeline.Status.Validating):
-                pass
-            else:
-                raise RuntimeError(f"Tx rejected: {event.status}")
-
-
 cfg = json.loads(open("./config.json").read())
 cl = Client(cfg)
-
-domain = Domain("python")
-register = Register.identifiable(domain)
-hash = cl.submit_isi(register)
-wait_for_tx(cl, hash)
-
-asset_definition = asset.Definition(
-    "pyasset#python",
-    asset.ValueType.Quantity(),
-    asset.Mintable.Infinitely(),
-)
-register = Register.identifiable(asset_definition)
-hash = cl.submit_isi(register)
-wait_for_tx(cl, hash)
-
-keypair = KeyPair()
-acct = account.Account("monty@python", signatories=[keypair.public])
-register = Register.identifiable(acct)
-hash = cl.submit_isi(register)
-wait_for_tx(cl, hash)
-
-condition = account.SignatureCheckCondition(Expression.Equal(expression.Equal(Value.U32(1), Value.U32(1))))
-account_id = Expression(Value(Id(account.Id("monty", "python"))))
-mint = Mint(condition, account_id)
-hash = cl.submit_isi(mint)
-wait_for_tx(cl, hash)
 
 query = FindAssetById.id(asset.Id("rose#wonderland", "alice@wonderland"))
 print(cl.query(query))
