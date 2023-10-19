4fd1c35d44661835ea78b9496f506b8b9df5fc3c
OliveiraEdu <40703876+OliveiraEdu@users.noreply.github.com>
2023-10-19 18:01:18 -0300

    Update visualize_outputs.py

--- python_scripts/visualize_outputs.py
+++ python_scripts/visualize_outputs.py
@@ -1,7 +1,16 @@
 import json
+
 from iroha2 import Client
-from iroha2.data_model.isi import *
-from iroha2.data_model.domain import *
+
+from iroha2.data_model.isi import Register, Mint
+from iroha2.data_model.domain import Domain
+from iroha2.data_model.account import Account
+from iroha2.data_model import asset, account, expression, Value, Identifiable, Id
+from iroha2.data_model.expression import Expression
+from iroha2.data_model.events import FilterBox, pipeline, Event
+from iroha2.crypto import KeyPair
+from iroha2.data_model.query.asset import FindAssetById
+from iroha2.data_model.query import Queryel.domain import *
 
 cfg = json.loads(open("config.json").read())
 
