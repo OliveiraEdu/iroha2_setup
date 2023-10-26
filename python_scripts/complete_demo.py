3a26739f661233ae54d8d98fe3398ae1141e3638
OliveiraEdu <40703876+OliveiraEdu@users.noreply.github.com>
2023-10-26 17:21:21 -0300

    Update complete_demo.py

--- python_scripts/complete_demo.py
+++ python_scripts/complete_demo.py
@@ -13,63 +13,30 @@ from iroha2.crypto import KeyPair
 from iroha2.data_model.query.asset import FindAssetById
 from iroha2.data_model.query import Query
 
-
-def wait_for_tx(cl: Client, hash: str):
-    filter = FilterBox(
-        pipeline.EventFilter(
-            entity_kind=pipeline.EntityKind.Transaction(),
-            status_kind=None,
-            hash=None,
-        ))
-
-    listener = cl.listen(filter)
-
-    for event in listener:
-        if isinstance(event, Event.Pipeline) and event.hash == hash:
-            if isinstance(event.status, pipeline.Status.Committed):
-                return
-            elif isinstance(event.status, pipeline.Status.Validating):
-                pass
-            else:
-                raise RuntimeError(f"Tx rejected: {event.status}")
-
-
 cfg = json.loads(open("./config.json").read())
 cl = Client(cfg)
 
-domain = Domain("rabbit_hole")
+domain = Domain("beatles")
 register = Register.identifiable(domain)
 hash = cl.submit_isi(register)
-wait_for_tx(cl, hash)
 
-#asset_definition = asset.Definition(
-#    "jar_of_marmelade#rabbit_hole",
-#    asset.ValueType.Quantity(),
-#    asset.Mintable.Infinitely(),
-#)
-
-jar_of_marmelade = asset.Definition(
-    value_type=asset.ValueType.Quantity,
-    id=asset.DefinitionId(name="jar_of_marmelade", domain_name="rabbit_hole"),
-    metadata={"a": Value.U32(10)},
-    mintable=False
+asset_definition = asset.Definition(
+    "letitbe#beatles",
+    asset.ValueType.Quantity(),
+    asset.Mintable.Infinitely(),
 )
-
-register = Register.identifiable(jar_of_marmelade)
+register = Register.identifiable(asset_definition)
 hash = cl.submit_isi(register)
-wait_for_tx(cl, hash)
 
 keypair = KeyPair()
-acct = account.Account("dodo@rabbit_hole", signatories=[keypair.public])
+acct = account.Account("paul@beatles", signatories=[keypair.public])
 register = Register.identifiable(acct)
 hash = cl.submit_isi(register)
-wait_for_tx(cl, hash)
 
 condition = account.SignatureCheckCondition(Expression.Equal(expression.Equal(Value.U32(1), Value.U32(1))))
-account_id = Expression(Value(Id(account.Id("dodo", "rabbit_hole"))))
+account_id = Expression(Value(Id(account.Id("paul", "beatles"))))
 mint = Mint(condition, account_id)
 hash = cl.submit_isi(mint)
-wait_for_tx(cl, hash)
 
-query = FindAssetById.id(asset.Id("jar_of_marmelade#rabbit_hole", "dodo@rabbit_hole"))
+query = FindAssetById.id(asset.Id("rose#wonderland", "alice@wonderland"))
 print(cl.query(query))
