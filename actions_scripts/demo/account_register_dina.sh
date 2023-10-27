1464cfcda2a5ebc74cccf5bffc863f6532781f10
OliveiraEdu <40703876+OliveiraEdu@users.noreply.github.com>
2023-10-27 08:32:35 -0300

    Update account_register_dina.sh

--- actions_scripts/demo/account_register_dina.sh
+++ actions_scripts/demo/account_register_dina.sh
@@ -1,4 +1,8 @@
 ./iroha_client_cli account register \
     --id="dina@unijui" \
-    --key="ed0120a4c4dadd9f18b0f63d6a420151fe0748d785475dec63034a15fcf999ceda1e65"
+    --key="ed012020d82bf51ce2bc9b792a2e801b6609edfa163234c1a7e9439e09c2f23b3babaa"
+
+#Public key (multihash): ed012020d82bf51ce2bc9b792a2e801b6609edfa163234c1a7e9439e09c2f23b3babaa
+#Private key: a8390c42f52ced1db1e55a67205e3217c4c7ac228402fd2fe3c963c70cf5df9a20d82bf51ce2bc9b792a2e801b6609edfa163234c1a7e9439e09c2f23b3babaa
+#Digest function: ed25519
     
