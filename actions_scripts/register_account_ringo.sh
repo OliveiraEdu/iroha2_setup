5dcf6b7411cc5f079c23f6f9fb2adf42095cf844
OliveiraEdu <40703876+OliveiraEdu@users.noreply.github.com>
2023-10-27 08:25:15 -0300

    Add files via upload

---
+++ actions_scripts/register_account_ringo.sh
@@ -0,0 +1,6 @@
+./iroha_client_cli account register \
+    --id="ringo@beatles" \
+    --key="ed0120aee72ea42dcb3873fd3782835a8aced6ff915167360cf60745d533eaa409b4f8" 
+
+#Private key: 2cf75e8106f8412c493400e7bbeedc7387af58830c4c0c4884994cdb554cc94aaee72ea42dcb3873fd3782835a8aced6ff915167360cf60745d533eaa409b4f8
+#Digest function: ed25519
