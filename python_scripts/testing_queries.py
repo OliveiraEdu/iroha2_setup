05d9799e2ce7d175f4cf6dc28ca27d786b989725
eduardo <edurdocostaoliveira@gmail.com>
2023-11-12 17:32:38 -0300

    including the interactive menu app

--- python_scripts/testing_queries.py
+++ python_scripts/testing_queries.py
@@ -24,7 +24,7 @@ print(cl.query(query))
 alice_id = "alice@wonderland"
 asset_name = "pint"
 
-query = FindAssetsByAccountId(account.Id(alice_id))
+query = FindAssetsByAccountId.accountId(account.Id("alice@wonderland"))
 print(cl.query(query))
 
 
