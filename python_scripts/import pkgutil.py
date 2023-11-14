b4298420c22ba5e1a0a350e8cd70824c5157bdfa
eduardo <edurdocostaoliveira@gmail.com>
2023-11-14 20:07:47 -0300

    including the interactive menu app

---
+++ python_scripts/import pkgutil.py
@@ -0,0 +1,15 @@
+import pkgutil
+
+# Get a list of all installed modules
+installed_modules = [name for _, name, _ in pkgutil.iter_modules()]
+
+# Iterate through the modules and print their classes
+for module_name in installed_modules:
+    try:
+        module = __import__(module_name)
+        module_contents = dir(module)
+        classes_in_module = [name for name in module_contents if isinstance(getattr(module, name), type)]
+        if classes_in_module:
+            print(f"Classes in {module_name}: {classes_in_module}")
+    except ImportError as e:
+        print(f"Error importing module {module_name}: {e}")
