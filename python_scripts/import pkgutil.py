79c99e651a5816b61190e6c99e8de2ce0f8836bd
eduardo <edurdocostaoliveira@gmail.com>
2023-11-15 16:47:28 -0300

    including the interactive menu app

--- python_scripts/import pkgutil.py
+++ python_scripts/import pkgutil.py
@@ -1,4 +1,5 @@
 import pkgutil
+from icecream import ic
 
 # Get a list of all installed modules
 installed_modules = [name for _, name, _ in pkgutil.iter_modules()]
@@ -10,6 +11,6 @@ for module_name in installed_modules:
         module_contents = dir(module)
         classes_in_module = [name for name in module_contents if isinstance(getattr(module, name), type)]
         if classes_in_module:
-            print(f"Classes in {module_name}: {classes_in_module}")
+            ic(f"Classes in {module_name}: {classes_in_module}")
     except ImportError as e:
-        print(f"Error importing module {module_name}: {e}")
+        ic(f"Error importing module {module_name}: {e}")
